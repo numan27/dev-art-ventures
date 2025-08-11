f84afab2e068b96e841061313594f422ff37bd6b
Numan Khalil <numankhalil27@gmail.com>
2025-08-12 02:38:31 +0500

    Fixes: Updated deisgn and copy of Staff Augmentation page

--- app/(pages)/ventures/staff-augmentation/page.tsx
+++ app/(pages)/ventures/staff-augmentation/page.tsx
@@ -97,7 +97,7 @@ export default function StaffAugmentation() {
       </div>
       <Features />
       <HireTalent />
-      <Highlights />
+      {/* <Highlights /> */}
       <RolesWeProvide />
       <WhyChooseUs />
       <Testimonials />
