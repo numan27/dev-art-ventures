3b6ec010d9e40fa55fd948fc811870cf3ae8b6bf
Numan Khalil <numankhalil27@gmail.com>
2025-08-19 12:14:22 +0500

    Brand logos updated

--- app/_shared/components/common/brandSlider/index.tsx
+++ app/_shared/components/common/brandSlider/index.tsx
@@ -3,13 +3,37 @@ import classNames from "classnames";
 import styles from "./style.module.scss";
 
 const socialIcons = [
-  "/social-brand-1.png",
-  "/social-brand-2.png",
-  "/social-brand-3.png",
-  "/social-brand-4.png",
-  "/social-brand-5.png",
-  "/social-brand-6.png",
-  "/social-brand-7.png",
+  // Major Brand Logos (High Priority)
+  "/brands/adobe-xs.png",
+  "/brands/aws.png",
+  "/brands/figma.png",
+  "/brands/framer.png",
+  "/brands/zoom.png",
+  "/brands/google.png",
+
+  // Major Development Tools (Medium Priority)
+  "/tools/react.png",
+  "/tools/next.png",
+  "/tools/nest.png",
+  "/tools/angular.png",
+  "/tools/typescript.png",
+  "/tools/mongo-db.png",
+  "/tools/wordpress.png",
+  "/tools/vs-code.png",
+  "/tools/git.png",
+  "/tools/git-hub.png",
+  "/tools/vite.png",
+  "/tools/vercel.png",
+  "/tools/tailwind.png",
+
+  // Other Development Tools (Lower Priority)
+  "/tools/css.png",
+  "/tools/git-lab.png",
+  "/tools/html.png",
+  "/tools/jira.png",
+  "/tools/nuxt.png",
+  "/tools/php.png",
+  "/tools/svelte.png",
 ];
 
 const BrandSlider = () => {
