18361069b17fe8f4d6dfbc1c676a8f73002885b1
Numan Khalil <numankhalil27@gmail.com>
2025-10-09 11:45:25 +0500

    google tag manager code added

--- app/layout.tsx
+++ app/layout.tsx
@@ -106,6 +106,22 @@ export default function RootLayout({
         <link rel="icon" href={Images.Logo.src} />
         <link rel="apple-touch-icon" href={Images.Logo.src} />
 
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MRQCFXT8');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         {/* Meta Pixel Code */}
         {/* <script
           dangerouslySetInnerHTML={{
@@ -183,6 +199,14 @@ export default function RootLayout({
       </head>
 
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html:
+              '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MRQCFXT8" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
+          }}
+        />
+        {/* End Google Tag Manager (noscript) */}
         {/* Meta Pixel NoScript Fallback */}
         {/* <noscript>
           <img
